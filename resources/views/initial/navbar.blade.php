1aaa81c1c5858f0911fc22d9225037c41903b968
beat-kuliah <if-18056@students.ithb.ac.id>
2021-12-11 20:44:42 +0700

    first commit

--- resources/views/initial/navbar.blade.php
+++ resources/views/initial/navbar.blade.php
@@ -1,7 +1,7 @@
 @extends('initial.style')
 
 <html>
-    <div class="container">
+    <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/home">QURBANER</a>
